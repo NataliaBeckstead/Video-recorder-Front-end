2b5430a9cd836d0a7bb2c89a05d96aeee03e83ef
Natalia Beckstead <toosmiravilla@gmail.com>
2020-05-15 11:55:22 -0600

    created react app with simple video recorder

---
+++ video_recorder/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import VideoAssessment from "./components/VideoAssessment";
+
+function App() {
+  return (
+    <VideoAssessment />
+  );
+}
+
+export default App;
